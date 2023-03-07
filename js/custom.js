2878276e521aca8c828ba8a411c6a617bfc2bf32
Abdul Hadi <abdul.806@yahoo.com>
2023-03-08 02:35:38 +0500

    s

--- js/custom.js
+++ js/custom.js
@@ -203,3 +203,18 @@ $('.count').each(function () {
       }
   });
 });
+
+// loader here
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+      "body").style.visibility = "hidden";
+      document.querySelector(
+      "#loader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+      "#loader").style.display = "none";
+      document.querySelector(
+      "body").style.visibility = "visible";
+  }
+};
