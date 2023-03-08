f4a71bb3e310d528487f5803853dc5fb1343d946
Abdul Hadi <abdul.806@yahoo.com>
2023-03-09 02:48:43 +0500

    UpdatesUI

--- js/custom.js
+++ js/custom.js
@@ -284,22 +284,11 @@ document.onreadystatechange = function() {
 
 // header sticky
 // adding class to header on scroll
-function headerFixed() {
-  jQuery(window).scroll(function() {    
-    var scroll = jQuery(window).scrollTop();
-    var atfHeight = jQuery( ".banner" ).height();
-
-    if (scroll >= 50) {
-        jQuery(".header").addClass("position-fixed");
-    }else {
-        jQuery(".header").removeClass("position-fixed");
-    }
-
-    if ( scroll >= atfHeight - 600 ) {
-        jQuery(".atf-fix-buttons").addClass("change-color");
-    }else {
-        jQuery(".atf-fix-buttons").removeClass("change-color");
-    }
-
-  });
-}
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 100) {
+    $('.header').slideDown( "slow" ).addClass('fixed');
+   }
+   else {
+    $('.header').removeClass('fixed');
+   }
+});
